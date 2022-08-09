4deb74d93b00f9e707d11b3877fc32b693b1f9f1
ihciab <2137004827@qq.com>
2022-08-09 19:56:04 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<memory>
+#include<algorithm>
+#include<list>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+void main()
+{
+	cout << "选择方程种类";
+	//一元多次 多元一次 多元多次 一元微分方程 矩阵方程    2*x+2*y-2*z=1
+	while (1)
+	{
+
+		
+		if (_kbhit())
+		{
+			char choose = _getch();
+			
+			switch (choose)
+			{
+			case'a':
+
+
+			default:
+				break;
+			}
+		}
+
+
+}
